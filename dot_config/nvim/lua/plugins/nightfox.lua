364fbb45d8d54b0602ddc1febd98a01a0d5d74f4
Yuan Chiu <chyuaner@gmail.com>
2025-04-28 18:20:49 +0800

    feat nvim: 設定nordfox colorscheme

---
+++ dot_config/nvim/lua/plugins/nightfox.lua
@@ -0,0 +1,20 @@
+-- if true then return {} end
+return {
+  {
+    "EdenEast/nightfox.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("nightfox").setup({
+        options = {
+          styles = {
+            comments = "italic",
+            keywords = "bold",
+            types = "italic,bold",
+          },
+        },
+      })
+      vim.cmd("colorscheme nordfox")
+    end,
+  },
+}
