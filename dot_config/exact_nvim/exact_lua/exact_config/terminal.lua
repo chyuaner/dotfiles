8d427316bde4cd1fb307a7f60eafa528364668d1
Yuan Chiu <chyuaner@gmail.com>
2025-04-30 08:48:10 +0800

    feat nvim: add terminal color

---
+++ dot_config/exact_nvim/exact_lua/exact_config/terminal.lua
@@ -0,0 +1,17 @@
+-- 設置終端顏色
+vim.g.terminal_color_0  = "#3b4252"  -- 黑色
+vim.g.terminal_color_1  = "#bf616a"  -- 紅色
+vim.g.terminal_color_2  = "#a3be8c"  -- 綠色
+vim.g.terminal_color_3  = "#ebcb8b"  -- 黃色
+vim.g.terminal_color_4  = "#81a1c1"  -- 藍色
+vim.g.terminal_color_5  = "#b48ead"  -- 紫色
+vim.g.terminal_color_6  = "#88c0d0"  -- 青色
+vim.g.terminal_color_7  = "#d8dee9"  -- 白色
+vim.g.terminal_color_8  = "#3b4252"  -- 亮黑色（灰色）
+vim.g.terminal_color_9  = "#bf616a"  -- 亮紅色
+vim.g.terminal_color_10 = "#a3be8c" -- 亮綠色
+vim.g.terminal_color_11 = "#ebcb8b" -- 亮黃色
+vim.g.terminal_color_12 = "#81a1c1" -- 亮藍色
+vim.g.terminal_color_13 = "#b48ead" -- 亮紫色
+vim.g.terminal_color_14 = "#88c0d0" -- 亮青色
+vim.g.terminal_color_15 = "#d8dee9" -- 亮白色
--- dot_config/exact_nvim/exact_lua/init.lua
+++ dot_config/exact_nvim/exact_lua/init.lua
@@ -3,3 +3,4 @@ require("config.keymaps_nvim")
 require("config.keymaps")
 require("config.gui")
 require("config.lazy")
+require("config.terminal") -- 因為會被lazy.nvim override，所以要放在最後
