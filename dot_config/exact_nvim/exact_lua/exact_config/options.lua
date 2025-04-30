799779c907c3f60296fe1b2891283b63b4b8dde8
Yuan Chiu <chyuaner@gmail.com>
2025-04-30 08:55:39 +0800

    nvim 關閉swap當前目錄，改回nvim預設

--- dot_config/exact_nvim/exact_lua/exact_config/options.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/options.lua
@@ -3,7 +3,7 @@ vim.api.nvim_set_option("clipboard", "unnamedplus") -- 使用系統剪貼簿（W
 vim.opt.confirm = true      -- 操作過程有衝突時，以明確的文字來詢問
 vim.opt.history = 10000 -- 設定命令歷史記錄數量為 10000
 -- vim.opt.directory = "." -- 將暫存檔存放在當前資料夾中
-require("config.swap") -- 將暫存檔存放置位置另外做邏輯
+-- require("config.swap") -- 將暫存檔存放置位置另外做邏輯
 vim.opt.mouse = "a" -- 啟用滑鼠支援
 
 -- 編碼設定
