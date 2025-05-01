883103e2e66c566c79a0c7d819cb16679a4f90c7
Yuan Chiu <chyuaner@gmail.com>
2025-05-01 08:18:01 +0800

    nvim: add project.nvim & telescope

--- dot_config/nvim/exact_lua/exact_plugins/exact_components/file.lua
+++ dot_config/nvim/exact_lua/exact_plugins/exact_components/file.lua
@@ -1,5 +1,14 @@
+-- if true then return {} end
 return {
-  "ahmedkhalf/project.nvim",
+  -- 會自動cd到專案目錄
+  -- "ahmedkhalf/project.nvim",
+
+  -- 使用修改版的project.nvim
+  -- 因為此套件會噴警告，原作者已經沒再維護了
+  -- vim.lsp.buf_get_clients() is deprecated. Run ":checkhealth vim.deprecated" for more information
+  "Spelis/project.nvim",
+  branch = "patch-1",
+
   config = function()
     require("project_nvim").setup {
       -- your configuration comes here
---
+++ dot_config/nvim/exact_lua/exact_plugins/exact_components/telescope.lua
@@ -0,0 +1,6 @@
+-- if true then return {} end
+return {
+  -- 'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  'nvim-telescope/telescope.nvim', branch = '0.1.x',
+  dependencies = { 'nvim-lua/plenary.nvim' }
+}
