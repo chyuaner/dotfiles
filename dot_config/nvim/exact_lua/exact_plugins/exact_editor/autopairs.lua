b559a7f2efa7bf7bec3f45ddc94b539e211a2f95
Yuan Chiu <chyuaner@gmail.com>
2025-05-10 15:26:21 +0800

    nvim: 自動autopair補齊優化

--- dot_config/nvim/exact_lua/exact_plugins/exact_editor/autopairs.lua
+++ dot_config/nvim/exact_lua/exact_plugins/exact_editor/autopairs.lua
@@ -25,4 +25,20 @@ return {
       map_c_w = false, -- map <c-w> to delete a pair if possible
     },
   },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    config = true
+  },
+
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  },
 }
