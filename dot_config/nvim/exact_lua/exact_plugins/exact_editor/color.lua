5e0e677cb8edbc825ef8d32bfbc8a5128596b52a
Yuan Chiu <chyuaner@gmail.com>
2025-05-23 20:16:56 +0800

    nvim: color picker 更換顏色選擇器

--- dot_config/nvim/exact_lua/exact_plugins/exact_editor/color.lua
+++ dot_config/nvim/exact_lua/exact_plugins/exact_editor/color.lua
@@ -1,32 +1,42 @@
 return {
   -- 單純顯示顏色
   -- 不過以下ccc.nvim與oklch-color-picker.nvim都有此功能，故關閉
-  -- {
-  --   'norcalli/nvim-colorizer.lua',
-  --   lazy = false,
-  --   config = function()
-  --     -- Attaches to every FileType mode
-  --     require 'colorizer'.setup()
+  {
+    'norcalli/nvim-colorizer.lua',
+    lazy = false,
+    config = function()
+      -- Attaches to every FileType mode
+      require 'colorizer'.setup()
 
-  --     -- require("colorizer").setup({
-  --     --   '*'; -- Highlight all files, but customize some others.
-  --     --   '!vim'; -- Exclude vim from highlighting.
-  --     --   '!markdown'; -- Exclude markdown from highlighting.
-  --     --   '!text'; -- Exclude text from highlighting.
-  --     --   '!html'; -- Exclude html from highlighting.
-  --     --   '!css'; -- Exclude css from highlighting.
-  --     --   '!javascript'; -- Exclude javascript from highlighting.
-  --     --   '!typescript'; -- Exclude typescript from highlighting.
-  --     -- }, {
-  --     --   RGB = true;         -- #RGB hex codes
-  --     --   RRGGBB = true;     -- #RRGGBB hex codes
-  --     --   names = false;     -- "Name" codes like Blue or blue
-  --     --   RRGGBBAA = true;   -- #RRGGBBAA hex codes
-  --     --   rgb_fn = true;     -- CSS rgb() and rgba() functions
-  --     --   hsl_fn = true;     -- CSS hsl() and hsla() functions
-  --     -- })
-  --   end,
-  -- },
+      -- require("colorizer").setup({
+      --   '*'; -- Highlight all files, but customize some others.
+      --   '!vim'; -- Exclude vim from highlighting.
+      --   '!markdown'; -- Exclude markdown from highlighting.
+      --   '!text'; -- Exclude text from highlighting.
+      --   '!html'; -- Exclude html from highlighting.
+      --   '!css'; -- Exclude css from highlighting.
+      --   '!javascript'; -- Exclude javascript from highlighting.
+      --   '!typescript'; -- Exclude typescript from highlighting.
+      -- }, {
+      --   RGB = true;         -- #RGB hex codes
+      --   RRGGBB = true;     -- #RRGGBB hex codes
+      --   names = false;     -- "Name" codes like Blue or blue
+      --   RRGGBBAA = true;   -- #RRGGBBAA hex codes
+      --   rgb_fn = true;     -- CSS rgb() and rgba() functions
+      --   hsl_fn = true;     -- CSS hsl() and hsla() functions
+      -- })
+    end,
+  },
+  { "nvzone/volt", lazy = true },
+  {
+    "nvzone/minty",
+    cmd = { "Shades", "Huefy" },
+    keys = {
+      {
+        "<leader>v", "<cmd>Huefy<cr>", desc = "Color pick under cursor"
+      },
+    },
+  }
 
   -- {
   --   "uga-rosa/ccc.nvim",
@@ -61,20 +71,21 @@ return {
   --   end,
   -- }
 
-  {
-    "eero-lehtinen/oklch-color-picker.nvim",
-    enabled = require("tools.gui").is_graphical(),
-    event = "VeryLazy",
-    version = "*",
-    keys = {
-      -- One handed keymap recommended, you will be using the mouse
-      {
-        "<leader>v",
-        function() require("oklch-color-picker").pick_under_cursor() end,
-        desc = "Color pick under cursor",
-      },
-    },
-    ---@type oklch.Opts
-    opts = {},
-  }
+  -- {
+  --   "eero-lehtinen/oklch-color-picker.nvim",
+  --   enabled = require("tools.gui").is_graphical(),
+  --   event = "VeryLazy",
+  --   version = "*",
+  --   keys = {
+  --     -- One handed keymap recommended, you will be using the mouse
+  --     {
+  --       "<leader>v",
+  --       function() require("oklch-color-picker").pick_under_cursor() end,
+  --       desc = "Color pick under cursor",
+  --     },
+  --   },
+  --   ---@type oklch.Opts
+  --   opts = {},
+  -- },
+
 }
