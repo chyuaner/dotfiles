38c97c730d60748698bafce3fed9c2d34a928221
Yuan Chiu <chyuaner@gmail.com>
2025-05-31 14:32:32 +0800

    fix nvim: lsp 一直跳encoding警告問題

--- dot_config/nvim/exact_lua/lsp/init.lua
+++ dot_config/nvim/exact_lua/lsp/init.lua
@@ -2,3 +2,15 @@ vim.g.lsp_position_encoding = 'utf-16'
 require("lsp.config.mason")
 require("lsp.config.lspconfig")
 require("lsp.config.cmp")
+
+-- 處理nvim一直都會跳警告：
+-- position_encoding param is required in vim.lsp.util.make_position_params. Defaulting to position encoding of the first client.
+-- 目前無法處理，需要等待大多數來自 plugin 尚未更新以配合 Neovim 0.10+的套件修正，先手動抑制
+-- 臨時抑制這類 LSP 訊息
+vim.notify = function(msg, log_level, _)
+  if msg:match("position_encoding param is required") then
+    return
+  end
+  vim.api.nvim_notify(msg, log_level, {})
+end
+
