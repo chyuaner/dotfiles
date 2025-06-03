7ef8a4275d6878dd753916f6d7dfa2aa43390ee8
Yuan Chiu <chyuaner@gmail.com>
2025-06-03 23:12:44 +0800

    fix nvim: lsp/init.lua position_encoding param is required in vim.lsp.util.make_position_params. Defaulting to position encoding of the first client.

--- dot_config/nvim/exact_lua/lsp/init.lua
+++ dot_config/nvim/exact_lua/lsp/init.lua
@@ -7,10 +7,10 @@ require("lsp.config.cmp")
 -- position_encoding param is required in vim.lsp.util.make_position_params. Defaulting to position encoding of the first client.
 -- 目前無法處理，需要等待大多數來自 plugin 尚未更新以配合 Neovim 0.10+的套件修正，先手動抑制
 -- 臨時抑制這類 LSP 訊息
-vim.notify = function(msg, log_level, _)
-  if msg:match("position_encoding param is required") then
-    return
-  end
-  vim.api.nvim_notify(msg, log_level, {})
-end
+-- vim.notify = function(msg, log_level, _)
+--   if msg:match("position_encoding param is required") then
+--     return
+--   end
+--   vim.api.nvim_notify(msg, log_level, {})
+-- end
 
