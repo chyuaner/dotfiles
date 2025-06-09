e6eff2a8177b3fd444a5fe7653230176041b6dac
Yuan Chiu <chyuaner@gmail.com>
2025-06-09 21:18:57 +0800

    feat nvim: add neogit

--- dot_config/nvim/exact_lua/exact_plugins/exact_components/git.lua
+++ dot_config/nvim/exact_lua/exact_plugins/exact_components/git.lua
@@ -2,6 +2,19 @@ return {
   {'akinsho/git-conflict.nvim', version = "*", config = true},
   {'sindrets/diffview.nvim'},
   {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+      -- "ibhagwan/fzf-lua",              -- optional
+      -- "echasnovski/mini.pick",         -- optional
+      -- "folke/snacks.nvim",             -- optional
+    },
+  },
+  {
     "kdheepak/lazygit.nvim",
     lazy = true,
     cmd = {
